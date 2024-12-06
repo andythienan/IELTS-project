01f960e642344d47361a96e12dd7b54ff70a834c
andythienan <thienan18082006@gmail.com>
2024-12-06 13:38:45 +0700

    sửa phần express và đổi tên file thành ejs

--- app.js
+++ app.js
@@ -1,35 +1,21 @@
 const express = require('express');
-const morgan = require('morgan')
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const handlebars = require('express-handlebars');
 const path = require('path');
 const app = express();
 const port = 3000;
 
-//Template engine
-app.engine('html', handlebars.engine({
-    extname: '.html',    // cấu hình Handlebars để nhận diện file .html
-}));
-
-app.set('view engine', 'html'); 
+// View engine
+app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')));
-//Render
+
+// Declare routes
 app.get('/', (req, res) => {
-    res.render('homepage');
-  });
-  
-  app.get('/about-us', (req, res) => {
-    res.render('about-us');
-  });
-  
-  app.get('/exam-library', (req, res) => {
-    res.render('exam-library');
-  });
+  res.render('pages/homepage');
+});
+app.get('/about', (req, res) => {
+  res.render('pages/about');  
+});
 
-
-//Lắng nghe server
+// Chạy server
 app.listen(port, () => {
-    console.log(`Server đang lắng nghe tại http://localhost:${port}`);
-  });
+  console.log(`Server is running at http://localhost:${port}`);
+});
